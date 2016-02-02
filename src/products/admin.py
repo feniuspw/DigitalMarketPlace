cd16d77f5fd2f36840b03bd9f2b5be8ec4763c6d
feniuspw <leonardovillela.c@gmail.com>
2016-02-02 12:22:21 -0800

    edited admin.py to show different information about Products on admin page.

--- src/products/admin.py
+++ src/products/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Product
+
+#show more information on admin page of products
+#how stuff render or how stuff looks
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["__str__","description","price","sale_price"]
+    search_fields = ["title","description"]
+    list_filter = ["price","sale_price"]
+    list_editable = ["sale_price"]
+    class Meta:
+        model = Product
+
+
+
+
+
+
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+
