b0b69165a2da21158bbfdb095e857142f54e0432
feniuspw <leonardovillela.c@gmail.com>
2016-02-04 12:31:35 -0800

    Created first html pages. not_found.html not created yet.

--- src/products/views.py
+++ src/products/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
-
 # Create your views here.
+from .models import Product
+
+
+# one item
+def detail_view(request):
+    # html template
+    if request.user.is_authenticated():
+        template = "products/detail_view.html"
+        product = Product.objects.all().first()
+        # dictionary of values
+        context = {
+                   'object': product
+                   }
+    else:
+        template = "products/not_found.html"
+        context = {}
+    return render(request, template, context)
+
+
+# list of items
+def list_view(request):
+
+    # html template
+    template = "products/list_view.html"
+
+    queryset = Product.objects.all()
+
+    # dictionary of values
+    context={'queryset':queryset}
+    return render(request, template, context)
